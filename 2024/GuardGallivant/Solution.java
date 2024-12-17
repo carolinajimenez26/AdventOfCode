fdfff21747804d916900aa2875a13fb53e739edf
Carolina <carolina.jimenez.g@gmail.com>
2024-12-17 20:50:21 +0100

    Add Guard Gallivant problem part II

--- 2024/GuardGallivant/Solution.java
+++ 2024/GuardGallivant/Solution.java
@@ -7,12 +7,35 @@ import java.util.*;
 
 public class Solution {
   private static char[][] matrix = null;
+  private static char[][] original = null;
+  private static Set<Position> visitedPositions = new HashSet<>();
+  private static boolean isPrintEnabled = false;
 
   private static class Position {
     int row, col;
+    char dir = '^';
     Position(int row, int col) {
       this.row = row;
       this.col = col;
+    }
+
+    Position(int row, int col, char dir) {
+      this.row = row;
+      this.col = col;
+      this.dir = dir;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+      Position position = (Position) o;
+      return row == position.row && col == position.col && dir == position.dir;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(row, col, dir);
     }
   }
 
@@ -25,10 +48,120 @@ public class Solution {
 
     readFile(fileName);
     print(matrix);
-    solve();
+    // solvePartOne();
+    solvePartTwo();
   }
 
-  private static void solve() {
+  private static void solvePartTwo() {
+    final Position initialPos = findElement('^');
+    if (initialPos == null) {
+      System.out.println("Error, couldn't find element");
+      return;
+    }
+    int result = 0;
+    original = copy(matrix);
+
+    for (int row = 0; row < matrix.length; row++) {
+      for (int col = 0; col < matrix[row].length; col++) {
+        if (initialPos.row == row && initialPos.col == col) {
+          continue; // can't override guard initial position
+        }
+        if (isABlocker(row, col)) {
+          continue; // no need to add a blocker where already exists
+        }
+        if (isPrintEnabled) System.out.println("-------------------------");
+        if (isPrintEnabled) System.out.println("Adding blocker in position " + row + ", " + col);
+        matrix[row][col] = '#';
+        print(matrix);
+        if (hasALoop(initialPos.row, initialPos.col)) {
+          result++;
+        }
+        matrix = copy(original);
+        visitedPositions.clear();
+      }
+    }
+    System.out.println("Solution " + result);
+  }
+
+  private static char[][] copy(final char[][] target) {
+    char[][] result = new char[target.length][];
+    for (int i = 0; i < target.length; i++) {
+      // Allocate memory for each row and copy it
+      result[i] = new char[target[i].length];
+      System.arraycopy(target[i], 0, result[i], 0, target[i].length);
+    }
+    return result;
+  }
+
+  private static boolean hasALoop(int row, int col) {
+    Position currentPos = new Position(row, col);
+    while (!isInBorder(currentPos.row, currentPos.col)) {
+      currentPos = advancePartTwo(currentPos);
+      if (visitedPositions.contains(currentPos)) {
+        if (isPrintEnabled) System.out.println("Found a loop!");
+        return true;
+      }
+    }
+    if (isPrintEnabled) System.out.println("Didn't find a loop!");
+    return false;
+  }
+
+  private static Position advancePartTwo(final Position pos) {
+    if (isPrintEnabled) System.out.println("Adancing from " + pos.row + ", " + pos.col);
+    int row = pos.row, col = pos.col;
+    char dir = matrix[row][col];
+    boolean hasCycle = false;
+    switch(dir) {
+      case '^':
+        while (!isInBorder(row, col) && !isABlocker(row - 1, col) && !hasCycle) {
+          markStep(row, col);
+          row--;
+        }
+        matrix[row][col] = '>';
+        break;
+      case '>':
+        while (!isInBorder(row, col) && !isABlocker(row, col + 1)) {
+          markStep(row, col);
+          col++;
+        }
+        matrix[row][col] = 'v';
+        break;
+      case 'v':
+        while (!isInBorder(row, col) && !isABlocker(row + 1, col)) {
+          markStep(row, col);
+          row++;
+        }
+        matrix[row][col] = '<';
+        break;
+      case '<':
+        while (!isInBorder(row, col) && !isABlocker(row, col - 1)) {
+          markStep(row, col);
+          col--;
+        }
+        matrix[row][col] = '^';
+        break;
+    }
+    pos.row = row;
+    pos.col = col;
+    dir = matrix[row][col];
+    
+    return new Position(row, col, dir);
+  }
+
+  private static boolean isValidPosition(final int row, final int col) {
+    if (row >= 0 && row < matrix.length && col >= 0 && col < matrix.length) {
+      return true;
+    }
+    return false;
+  }
+
+  private static void markStep(final int row, final int col) {
+    final char dir = matrix[row][col];
+    final Position currentPos = new Position(row, col, dir);
+    visitedPositions.add(currentPos);
+  }
+
+  private static void solvePartOne() {
     int result = 0;
     Position currentPos = findElement('^');
     if (currentPos == null) {
@@ -36,7 +169,7 @@ public class Solution {
       return;
     }
     while (!isInBorder(currentPos.row, currentPos.col)) {
-      currentPos = advance(currentPos);
+      currentPos = advancePartOne(currentPos);
     }
     result = countElements('X') + 1;
     System.out.println("Solution " + result);
@@ -54,7 +187,7 @@ public class Solution {
     return result;
   }
 
-  private static Position advance(final Position pos) {
+  private static Position advancePartOne(final Position pos) {
     System.out.println("Adancing from " + pos.row + ", " + pos.col);
     int row = pos.row, col = pos.col;
     char dir = matrix[row][col];
@@ -95,26 +228,26 @@ public class Solution {
   }
 
   private static boolean isABlocker(final int row, final int col) {
-    if (row < 0 || row >= matrix.length || col < 0 || col >= matrix.length) {
+    if (!isValidPosition(row, col)) {
       return true;
     }
     char target = matrix[row][col];
     boolean result = target == '#';
-    System.out.println("isABlocker? " + row + ", " + col + " " + target + ", " + result);
+    if (isPrintEnabled) System.out.println("isABlocker? " + row + ", " + col + " " + target + ", " + result);
     return result;
   }
 
   private static boolean isInBorder(final int row, final int col) {
-    System.out.println("isInBorder? " + row + ", " + col);
+    if (isPrintEnabled) System.out.println("isInBorder? " + row + ", " + col);
     if (row == 0 || row == matrix.length - 1) {
-      System.out.println("Yes, is in row border");
+      if (isPrintEnabled) System.out.println("Yes, is in row border");
       return true;
     }
     if (col == 0 || col == matrix.length - 1) {
-      System.out.println("Yes, is in col border");
+      if (isPrintEnabled) System.out.println("Yes, is in col border");
       return true;
     }
-    System.out.println("No, is not in a border");
+    if (isPrintEnabled) System.out.println("No, is not in a border");
     return false;
   }
 
@@ -147,6 +280,9 @@ public class Solution {
   }
 
   private static void print(final char[][] matrix) {
+    if (!isPrintEnabled) {
+      return;
+    }
     // Print the matrix to verify
     if (matrix != null) {
       for (char[] row : matrix) {
